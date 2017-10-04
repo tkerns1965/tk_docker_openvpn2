fc8d0c8937b8ac748b89e288aa5f66a5fc632297
tkerns1965 <tony.kerns.65@gmail.com>
2017-10-04 16:13:19 -0400

    Create build_tkdovpn2.sh

---
+++ build_tkdovpn2.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+cd ./easyrsa_base/
+docker build -t img_tkdovpn2_easyrsa_base .
+cd ../openvpn_base/
+docker build -t img_tkdovpn2_openvpn_base .
+# cd ../easyrsa/
+# docker build -t img_tkdovpn2_easyrsa .
+# cd ../server/
+# docker build -t img_tkdovpn2_server .
+# cd ../client/
+# docker build -t img_tkdovpn2_client .
+cd ../
+
+# docker network create net_tkdovpn2_bridge
