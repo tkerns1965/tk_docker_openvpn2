71283e2029fafa4ba39c40e7fdb600026bb37376
tkerns1965 <tony.kerns.65@gmail.com>
2017-10-04 16:16:14 -0400

    Update build_tkdovpn2.sh

--- build_tkdovpn2.sh
+++ build_tkdovpn2.sh
@@ -2,10 +2,10 @@
 
 cd ./easyrsa_base/
 docker build -t img_tkdovpn2_easyrsa_base .
-cd ../openvpn_base/
-docker build -t img_tkdovpn2_openvpn_base .
-# cd ../easyrsa/
-# docker build -t img_tkdovpn2_easyrsa .
+# cd ../openvpn_base/
+# docker build -t img_tkdovpn2_openvpn_base .
+cd ../easyrsa/
+docker build -t img_tkdovpn2_easyrsa .
 # cd ../server/
 # docker build -t img_tkdovpn2_server .
 # cd ../client/
