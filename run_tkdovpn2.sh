72a3a2e3582e53dffb4c8bd1cb9fd382a8fdee2c
tkerns1965 <tony.kerns.65@gmail.com>
2017-10-05 09:32:15 -0400

    create run_tkdovpn2.sh

---
+++ run_tkdovpn2.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+docker run -d --name openvpn_svr --network net_tkdovpn2_bridge \
+  --mount src=vol_tkdovpn2_svr_pki,dst=/etc/easyrsa/pki \
+  --cap-add=NET_ADMIN --device=/dev/net/tun -p 1194:1194/udp \
+  --entrypoint "openvpn /etc/openvpn/server.conf"
+  img_tkdovpn2_server
+
+# docker run -d --name openvpn_clt1 --network net_tkdovpn2_bridge \
+  # --mount src=vol_tkdovpn2_clt_pki,dst=/etc/easyrsa/pki \
+  # --cap-add=NET_ADMIN --device=/dev/net/tun \
+  # --entrypoint "openvpn /etc/openvpn/client.conf" \
+  # img_tkdovpn1_client
