a5a00639be97a03e9f49f75558dd98e9e7186f1e
tkerns1965 <tony.kerns.65@gmail.com>
2017-10-04 22:57:32 -0400

    Update configure_tkdovpn2.sh

--- configure_tkdovpn2.sh
+++ configure_tkdovpn2.sh
@@ -2,3 +2,7 @@
 
 docker run --mount src=vol_tkdovpn2_ca_pki,dst=/etc/easyrsa/pki --rm \
   img_tkdovpn2_easyrsa ./build_ca_and_gen_dh.sh
+docker run --mount src=vol_tkdovpn2_svr_pki,dst=/etc/easyrsa/pki --rm \
+  img_tkdovpn2_server ./gen_req.sh openvpn_svr
+docker run --mount src=vol_tkdovpn2_clt_pki,dst=/etc/easyrsa/pki --rm \
+  img_tkdovpn2_client ./gen_req.sh openvpn_clt1
