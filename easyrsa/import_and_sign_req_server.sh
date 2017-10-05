879ef850892a2a15a6ad90dceb97ce566679d1f0
tkerns1965 <tony.kerns.65@gmail.com>
2017-10-05 09:02:42 -0400

    Update import_and_sign_req_server.sh

--- easyrsa/import_and_sign_req_server.sh
+++ easyrsa/import_and_sign_req_server.sh
@@ -10,7 +10,7 @@ fi
 cd /etc/easyrsa/
 
 ./easyrsa import-req /svr_pki/reqs/$COMMON_NAME.req $COMMON_NAME
-./easyrsa sign-req server $COMMON_NAME
+echo "yes" | bash ./easyrsa sign-req server $COMMON_NAME
 
 cp /etc/easyrsa/pki/ca.crt /svr_pki/ca.crt
 cp /etc/easyrsa/pki/dh.pem /svr_pki/dh.pem
