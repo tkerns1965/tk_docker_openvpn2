a7165519442015777554f447a39728fde668a910
tkerns1965 <tony.kerns.65@gmail.com>
2017-10-05 09:03:11 -0400

    Update import_and_sign_req_client.sh

--- easyrsa/import_and_sign_req_client.sh
+++ easyrsa/import_and_sign_req_client.sh
@@ -10,7 +10,7 @@ fi
 cd /etc/easyrsa/
 
 ./easyrsa import-req /clt_pki/reqs/$COMMON_NAME.req $COMMON_NAME
-./easyrsa sign-req client $COMMON_NAME
+echo "yes" | bash ./easyrsa sign-req client $COMMON_NAME
 
 cp /etc/easyrsa/pki/ca.crt /clt_pki/ca.crt
 cp /etc/easyrsa/pki/issued/$COMMON_NAME.crt /clt_pki/$COMMON_NAME.crt
